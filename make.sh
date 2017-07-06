4e7bda854c7f928c17c1fb868e3347210e669efe
Baard H. Rehn Johansen <baard.johansen@bouvet.no>
2017-07-06 23:43:30 +0200

    wrong arch name

--- make.sh
+++ make.sh
@@ -2,13 +2,13 @@
 set -x
 TAG=$TRAVIS_TAG
 mkdir -p dist
-for arch in amd64 i386; do
+for arch in amd64 386; do
   GOOS=windows GOARCH=$arch go build -o sesam.exe main.go
   zip dist/sesam$TAG.windows-$arch.zip sesam.exe
   rm sesam.exe
 done
 for os in darwin linux; do
-  for arch in amd64 i386; do
+  for arch in amd64 386; do
   GOOS=$os GOARCH=$arch go build -o sesam main.go
   tar -zcf dist/sesam$TAG.$os-$arch.tar.gz sesam
   rm sesam
