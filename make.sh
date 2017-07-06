f1790d93da667975125ca24c106513f55bfa42e1
Baard H. Rehn Johansen <baard.johansen@bouvet.no>
2017-07-06 23:41:24 +0200

    support more archs

--- make.sh
+++ make.sh
@@ -2,12 +2,14 @@
 set -x
 TAG=$TRAVIS_TAG
 mkdir -p dist
-GOOS=windows GOARCH=amd64 go build -o sesam.exe main.go
-zip dist/sesam$TAG.windows-amd64.zip sesam.exe
-rm sesam.exe
-GOOS=darwin GOARCH=amd64 go build -o sesam main.go
-tar -zcf dist/sesam$TAG.darwin-amd64.tar.gz sesam
-rm sesam
-GOOS=linux GOARCH=amd64 go build -o sesam main.go
-tar -zcf dist/sesam$TAG.linux-amd64.tar.gz sesam
-rm sesam
+for arch in amd64 i386; do
+  GOOS=windows GOARCH=$arch go build -o sesam.exe main.go
+  zip dist/sesam$TAG.windows-$arch.zip sesam.exe
+  rm sesam.exe
+done
+for os in darwin linux; do
+  for arch in amd64 i386; do
+  GOOS=$os GOARCH=$arch go build -o sesam main.go
+  tar -zcf dist/sesam$TAG.$os-$arch.tar.gz sesam
+  rm sesam
+done
