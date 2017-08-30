17c5255fee853ff881e31fd3e3dc9e0af43ed4fe
Baard H. Rehn Johansen <baard.johansen@bouvet.no>
2017-08-30 21:29:12 +0200

    add installer script

---
+++ install-latest.sh
@@ -0,0 +1,2 @@
+URL=$( curl -s https://api.github.com/repos/sesam-io/sesam/releases/latest | jq -r '.assets[] | select(.name|endswith("linux-amd64.tar.gz")) | .browser_download_url')
+curl -L "$URL" | tar -xz
