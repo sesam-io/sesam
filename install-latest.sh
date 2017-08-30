9f98f5b5ce14c38692a7278b5f4be911b4227e0e
Baard H. Rehn Johansen <baard.johansen@bouvet.no>
2017-08-30 22:01:47 +0200

    use egrep instead

--- install-latest.sh
+++ install-latest.sh
@@ -1,4 +1,4 @@
 set -x
-URL=$( curl -s https://api.github.com/repos/sesam-io/sesam/releases/latest | jq -r '.assets[] | select(.name|endswith("linux-amd64.tar.gz")) | .browser_download_url')
-curl -L "$URL" | tar -xz
+curl -s https://api.github.com/repos/sesam-io/sesam/releases/latest | egrep -o '/sesam-io/sesam/releases/download/.*/.*.linux-amd64.tar.gz' | wget --base=http://github.com -i - -O sesam.tar.gz
+tar -xf sesam.tar.gz
 ./sesam -version
