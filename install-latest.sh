fee9c7c66621e7b065f8605ffd39c69e237f9fc5
Baard H. Rehn Johansen <baard.johansen@bouvet.no>
2017-08-30 21:53:08 +0200

    debug installer

--- install-latest.sh
+++ install-latest.sh
@@ -1,3 +1,4 @@
+set -x
 URL=$( curl -s https://api.github.com/repos/sesam-io/sesam/releases/latest | jq -r '.assets[] | select(.name|endswith("linux-amd64.tar.gz")) | .browser_download_url')
 curl -L "$URL" | tar -xz
 ./sesam -version
