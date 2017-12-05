e9a303981ddb4bb79fb3c6e534d9902324ec656d
BaardBouvet <baard.johansen@bouvet.no>
2017-12-05 13:43:18 +0100

    Move to more stable build

--- install-latest.sh
+++ install-latest.sh
@@ -1,5 +1,5 @@
 set -x
-TAG=${SESAM_TAG:-0.0.18}
+TAG=${SESAM_TAG:-0.0.22}
 wget -O sesam.tar.gz https://github.com/sesam-io/sesam/releases/download/$TAG/sesam$TAG.linux-amd64.tar.gz
 tar -xf sesam.tar.gz
 ./sesam -version
